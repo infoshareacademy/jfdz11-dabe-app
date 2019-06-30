083ebca1fdc454cace552b50aa2e824a79468983
jagda <danielzychniewicz@wp.pl>
2019-06-30 20:09:10 +0200

    Add navigation component

---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import MenuIcon from "@material-ui/icons/Menu";
+import Home from "@material-ui/icons/Home";
+import Equalizer from "@material-ui/icons/Equalizer";
+import PieChart from "@material-ui/icons/PieChart";
+import Style from "@material-ui/icons/Style";
+
+const useStyles = makeStyles({
+  list: {
+    width: 250
+  }
+});
+
+export default function Menu() {
+  const classes = useStyles();
+  const [state, setState] = React.useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false
+  });
+  const icons = [<Home />, <Style />, <Equalizer />, <PieChart />];
+
+  const toggleDrawer = (side, open) => event => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+
+    setState({ ...state, [side]: open });
+  };
+
+  const sideList = side => (
+    <div
+      className={classes.list}
+      role="presentation"
+      onClick={toggleDrawer(side, false)}
+      onKeyDown={toggleDrawer(side, false)}
+    >
+      <List>
+        {["Operacje", "Przegląd", "Podsumowanie", "Raporty"].map(
+          (text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{icons[index]}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          )
+        )}
+      </List>
+      <Divider />
+    </div>
+  );
+
+  return (
+    <div>
+      <MenuIcon onClick={toggleDrawer("left", true)} />
+      <Drawer open={state.left} onClose={toggleDrawer("left", false)}>
+        {sideList("left")}
+      </Drawer>
+    </div>
+  );
+}

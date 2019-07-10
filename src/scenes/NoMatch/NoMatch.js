aa4591c246bdb4d9aea717e076812a5fe7a4b65d
jagda <danielzychniewicz@wp.pl>
2019-07-11 00:12:22 +0200

    Update nomatch component

--- src/scenes/NoMatch/NoMatch.js
+++ src/scenes/NoMatch/NoMatch.js
@@ -7,7 +7,7 @@ function NoMatch() {
     <div className={styles.container}>
       <h1 className={styles.bigTitle}>404</h1>
       <Link className={styles.link} to="/">
-        Go back to App
+        Go back to dashboard.
       </Link>
     </div>
   );

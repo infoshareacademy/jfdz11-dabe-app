0dced6456688470ae9de4a9c6b2f5b0ef9c62c42
jagda <danielzychniewicz@wp.pl>
2019-07-11 10:56:07 +0200

    Add delete function to ReviewList component

--- src/components/ReviewList/ReviewList.js
+++ src/components/ReviewList/ReviewList.js
@@ -1,60 +1,27 @@
 import React, { useState } from "react";
 import MaterialTable from "material-table";
 
-export default function ReviewList({
-  selectedMonth,
-  expenses,
-  onRemoveExpense
-}) {
-  const [state, setState] = useState({
-    columns: [
-      { title: "Date", field: "date" },
-      { title: "Category", field: "category" },
-      { title: "Title", field: "title" },
-      { title: "Cost", field: "cost" }
-    ],
-    data: expenses
-      .filter(expens => expens.monthYear === selectedMonth.monthYear)
-      .map(expens => ({
-        date: expens.date.slice(0, 10),
-        cost: expens.cost,
-        title: expens.title,
-        category: expens.category
-      }))
-  });
+export default function ReviewList({ expenses, onRemoveExpense }) {
+  const columns = [
+    { title: "ID", field: "id" },
+    { title: "Date", field: "date" },
+    { title: "Category", field: "category" },
+    { title: "Title", field: "title" },
+    { title: "Cost", field: "cost" }
+  ];
 
   return (
     <MaterialTable
       title="All financial operations"
-      columns={state.columns}
-      data={state.data}
+      columns={columns}
+      data={expenses}
       editable={{
-        onRowAdd: newData =>
-          new Promise(resolve => {
-            setTimeout(() => {
-              resolve();
-              const data = [...state.data];
-              data.push(newData);
-              setState({ ...state, data });
-            }, 600);
-          }),
-        onRowUpdate: (newData, oldData) =>
-          new Promise(resolve => {
-            setTimeout(() => {
-              resolve();
-              const data = [...state.data];
-              data[data.indexOf(oldData)] = newData;
-              setState({ ...state, data });
-            }, 600);
-          }),
         onRowDelete: oldData =>
           new Promise(resolve => {
             setTimeout(() => {
+              onRemoveExpense(oldData.id);
               resolve();
-              const data = [...state.data];
-              data.splice(data.indexOf(oldData), 1);
-              setState({ ...state, data });
-            }, 600);
+            }, 0);
           })
       }}
     />

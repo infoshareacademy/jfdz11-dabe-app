31eed114486ac70959d2c92d150474bb27803429
jagda <danielzychniewicz@wp.pl>
2019-07-31 12:37:55 +0200

    Add linear progress component and profile component  style update

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,5 +1,7 @@
 import React, { useContext } from "react";
 import { withStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import CssBaseline from "@material-ui/core/CssBaseline";
 import Avatar from "@material-ui/core/Avatar";
 import Typography from "@material-ui/core/Typography";
 import avatarPlaceholder from "../../assets/avatar-placeholder.jpg";
@@ -9,71 +11,103 @@ import AddIcon from "@material-ui/icons/Add";
 import RemoveIcon from "@material-ui/icons/Remove";
 import ImageIcon from "@material-ui/icons/Image";
 import { AuthContext } from "../../contexts/AuthContext";
+import LinearProgress from "@material-ui/core/LinearProgress";
 
-const styles = () => ({
+const styles = theme => ({
+  main: {
+    width: "auto",
+    display: "block",
+    marginLeft: theme.spacing(3),
+    marginRight: theme.spacing(3),
+    [theme.breakpoints.up(400 + theme.spacing(3))]: {
+      width: 400,
+      marginLeft: "auto",
+      marginRight: "auto"
+    }
+  },
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    padding: `${theme.spacing(2)}px ${theme.spacing(3)}px ${theme.spacing(3)}px`
+  },
+  typography: {
+    padding: `${theme.spacing(2)}px ${theme.spacing(3)}px ${theme.spacing(3)}px`
+  },
   bigAvatar: {
     width: 60,
     height: 60,
-    margin: "0 auto"
+    margin: "0 auto 10px"
+  },
+  linearProgress: {
+    width: "100%",
+    height: 20
   }
 });
 
 function Profile(props) {
   const authContext = useContext(AuthContext);
   const { classes } = props;
+
   return (
-    <>
-      <Typography component="h1" variant="h4">
-        Profile
-      </Typography>
-      <div style={{ margin: "20px" }}>
+    <main className={classes.main}>
+      <CssBaseline />
+      <Paper className={classes.paper}>
+        <Typography className={classes.typography} component="h1" variant="h4">
+          Profile
+        </Typography>
         <Avatar
           src={
             authContext.avatarUrl ? authContext.avatarUrl : avatarPlaceholder
           }
           className={classes.bigAvatar}
         />
-        <div style={{ margin: "20px" }}>
-          <input
-            accept="image/*"
-            className={classes.input}
-            style={{ display: "none" }}
-            id="raised-button-file"
-            multiple
-            type="file"
-            onChange={event => authContext.setFile(event.target.files[0])}
-          />
-          <label htmlFor="raised-button-file">
-            <Fab size="small" color="default" component="span">
-              <ImageIcon />
-            </Fab>
-          </label>
+        <input
+          type="file"
+          accept="image/*"
+          style={{ display: "none" }}
+          id="raised-button-file"
+          multiple
+          onChange={event => authContext.setFile(event.target.files[0])}
+        />
+        <label htmlFor="raised-button-file">
+          <Fab size="small" color="secondary" component="span">
+            <ImageIcon />
+          </Fab>
+        </label>
+        <Typography component="p" variant="caption">
           {authContext.file && `${authContext.file.name}`}
-          <Fab
-            size="small"
-            color="primary"
-            component="span"
-            onClick={authContext.addAvatar}
-          >
-            <AddIcon />
-          </Fab>
-          <Fab
-            size="small"
-            color="secondary"
-            component="span"
-            onClick={authContext.removeAvatar}
-          >
-            <RemoveIcon />
-          </Fab>
-        </div>
-      </div>
-      <Typography component="h3" variant="h6">
-        {(authContext.user && authContext.user.email) || "email"}
-      </Typography>
-      <Typography component="h3" variant="h6">
-        {(authContext.user && authContext.user.uid) || "user name"}
-      </Typography>
-      <div style={{ margin: "20px" }}>
+        </Typography>
+        <LinearProgress
+          variant="determinate"
+          color="secondary"
+          value={authContext.completed}
+          className={classes.linearProgress}
+        />
+        <Fab
+          size="small"
+          color="primary"
+          component="span"
+          onClick={authContext.addAvatar}
+        >
+          <AddIcon />
+        </Fab>
+        <Fab
+          size="small"
+          color="secondary"
+          component="span"
+          onClick={authContext.removeAvatar}
+        >
+          <RemoveIcon />
+        </Fab>
+        <Typography component="h3" variant="h6">
+          {(authContext.user && authContext.user.email) || "email"}
+        </Typography>
+        <Typography component="h3" variant="h6">
+          {(authContext.user && authContext.user.uid) || "user name"}
+        </Typography>
+
         <Fab
           size="small"
           color="primary"
@@ -82,8 +116,8 @@ function Profile(props) {
         >
           <EditIcon />
         </Fab>
-      </div>
-    </>
+      </Paper>
+    </main>
   );
 }
 

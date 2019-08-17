13bd15e350029d56c5fd0fe49371fdfe9d290ae5
jagda <danielzychniewicz@wp.pl>
2019-08-17 22:48:26 +0200

    Move content from Dashboard to Finances, implement feature to close monthly budget

--- src/scenes/Dashboard/Dashboard.js
+++ src/scenes/Dashboard/Dashboard.js
@@ -1,33 +1,5 @@
 import React from "react";
-import { dashboard, charts, piechart, barchart } from "./Dashboard.module.css";
-import {
-  NewOperationForm,
-  BudgetInput,
-  ListOfExpenses,
-  ChartPie,
-  ChartBar,
-  MonthYearPicker
-} from "../../components";
 
 export default function Dashboard(props) {
-  return (
-    <div className={dashboard}>
-      <div>
-        <MonthYearPicker />
-        <BudgetInput />
-        <NewOperationForm />
-      </div>
-      <div>
-        <ListOfExpenses />
-      </div>
-      <div className={charts}>
-        <div className={barchart}>
-          <ChartBar />
-        </div>
-        <div className={piechart}>
-          <ChartPie />
-        </div>
-      </div>
-    </div>
-  );
+  return <></>;
 }

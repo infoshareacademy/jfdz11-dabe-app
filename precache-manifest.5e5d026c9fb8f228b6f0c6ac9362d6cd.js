bf4daa57cf019dd255fb45e6d8843e78991db398
jagda <danielzychniewicz@wp.pl>
2019-09-01 12:55:58 +0200

    Updates

---
+++ precache-manifest.5e5d026c9fb8f228b6f0c6ac9362d6cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "190ec7df027e78141325906c6ffda225",
+    "url": "/infoshareacademy/jfdz11-dabe-app/index.html"
+  },
+  {
+    "revision": "672f8123b2255fc40446",
+    "url": "/infoshareacademy/jfdz11-dabe-app/static/css/main.bfd4ce68.chunk.css"
+  },
+  {
+    "revision": "e1ac7b258affe0e2fd23",
+    "url": "/infoshareacademy/jfdz11-dabe-app/static/js/2.417ffb76.chunk.js"
+  },
+  {
+    "revision": "672f8123b2255fc40446",
+    "url": "/infoshareacademy/jfdz11-dabe-app/static/js/main.204de5fa.chunk.js"
+  },
+  {
+    "revision": "3c93cd622499679a9f1d",
+    "url": "/infoshareacademy/jfdz11-dabe-app/static/js/runtime~main.283403ff.js"
+  },
+  {
+    "revision": "e696fdffd1b7e9ab31a3e9460b57afca",
+    "url": "/infoshareacademy/jfdz11-dabe-app/static/media/avatar-placeholder.e696fdff.jpg"
+  }
+]);
